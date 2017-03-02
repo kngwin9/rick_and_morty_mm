5a01b5ccd2f4482f8fa267f2f34dfa431c42279b
Kevin Nguyen <kngwin9@gmail.com>
2017-03-02 10:11:01 -0800

    Got card flip function to work, going to add the comparrison next.

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-/**
- * Created by kngwin9 on 2/23/17.
- */
+// Created by kngwin9 on 2/23/17
 
 // v0.5 Global variables
 var first_card_clicked = null;
@@ -14,30 +12,62 @@ var attempts = 0;
 var accuracy = 0;
 var games_played = 0;
 
+$(document).ready(function(){
+
+    $(".back").click(function(){
+        card_clicked();
+        card_flipped(this);
+    });
+
+    /*  function initialGame() {
+
+     }
+
+     function initialClickHandler() {
+     $(".back").click(handleClick);
+     }
+
+     */
+
+    function card_flipped(card1, card2) {
+        $(card1).addClass('flipped');
+        $(card2).addClass('flipped');
+        console.log("Card flipped.");
+    }
+
+
     // Function that displays the stats
-    function display_stats () {
+    function display_stats() {
 
     }
 
     // Function that resets the stats
-    function reset_stats () {
+    function reset_stats() {
 
     }
+
     // Function for reset button
-    function reset_button () {
+    function reset_button() {
 
     }
+
 
     // Show card when clicked function
-    function card_clicked () {
-        if ( first_card_clicked === null ) {
-            first_card_clicked; // Assign to DOM Element
+    function card_clicked(card1, card2) {
+       // card_flipped(card1);
+
+        if (first_card_clicked === null) {
+            console.log("The first card was clicked.");
+            first_card_clicked = card1;
         }
         else {
-            second_card_click; // Assign to DOM Element
-            if ( first_card_clicked === second_card_click ) {
+            console.log("The second card was clicked.");
+            second_card_click = card2;
+            if (first_card_clicked === second_card_click) {
+                console.log("They match!");
+
                 // Add increment match counter
-                if ( match_counter === total_possible_matches ) {
+                if (match_counter === total_possible_matches) {
                     // Add display message to user
                 }
                 else {
@@ -52,3 +82,7 @@ var games_played = 0;
         }
 
     }
+
+});
+
+
